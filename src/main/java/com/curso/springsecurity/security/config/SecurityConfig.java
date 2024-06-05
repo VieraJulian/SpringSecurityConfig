b8ad309d11c50dd51ae8009ba6b2a8c661d0b0df
VieraJulian <julianviera2019@hotmail.com>
2024-06-05 18:24:45 -0300

    Configuración media de Spring Security

--- src/main/java/com/curso/springsecurity/security/config/SecurityConfig.java
+++ src/main/java/com/curso/springsecurity/security/config/SecurityConfig.java
@@ -2,9 +2,25 @@ package com.curso.springsecurity.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -13,14 +29,57 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .authorizeHttpRequests()
-                    .requestMatchers("/helloNoSecured").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().permitAll()
-                .and()
-                .httpBasic()
-                .and()
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    http.requestMatchers(HttpMethod.GET, "/helloNoSecured").permitAll();
+                    http.requestMatchers(HttpMethod.GET, "/helloSecured").hasAuthority("READ");
+                    http.anyRequest().denyAll();
+                })
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        List userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(User.withUsername("ADMIN")
+                                .password("1234")
+                                .roles("ADMIN")
+                                .authorities("READ", "CREATE", "UPDATE", "DELETE")
+                                .build());
+
+        userDetailsList.add(User.withUsername("INVITED")
+                .password("1234")
+                .roles("INVITED")
+                .authorities("READ")
+                .build());
+
+        userDetailsList.add(User.withUsername("USER")
+                .password("1234")
+                .roles("USER")
+                .authorities("UPDATE")
+                .build());
+
+        return new InMemoryUserDetailsManager(userDetailsList);
     }
 }
