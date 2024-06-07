8b7097c97c8147b83ffe633e5843b6ad534ad785
VieraJulian <julianviera2019@hotmail.com>
2024-06-07 12:44:44 -0300

    Controllador de Permissions, terminado

--- src/main/java/com/curso/springsecurity/controller/PermissionController.java
+++ src/main/java/com/curso/springsecurity/controller/PermissionController.java
@@ -1,7 +1,39 @@
 package com.curso.springsecurity.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.curso.springsecurity.model.Permission;
+import com.curso.springsecurity.repository.IPermissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/permissions")
 public class PermissionController {
+
+    @Autowired
+    private IPermissionRepository permissionRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Permission>> getAllPermissions(){
+        List<Permission> permissions = permissionRepository.findAll();
+
+        return ResponseEntity.ok(permissions);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Permission> getPermissionsById(@PathVariable Long id){
+        Optional<Permission> permission = permissionRepository.findById(id);
+
+        return permission.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Permission> createPermission(@RequestBody Permission permission){
+        return ResponseEntity.ok(permissionRepository.save(permission));
+    }
 }
+
+
