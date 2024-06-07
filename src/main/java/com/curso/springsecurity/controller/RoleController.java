bf3e5bec7a05fd80242f3581c076293973167c44
VieraJulian <julianviera2019@hotmail.com>
2024-06-07 16:53:46 -0300

    Controllador de Roles, terminado

--- src/main/java/com/curso/springsecurity/controller/RoleController.java
+++ src/main/java/com/curso/springsecurity/controller/RoleController.java
@@ -1,7 +1,54 @@
 package com.curso.springsecurity.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.curso.springsecurity.model.Permission;
+import com.curso.springsecurity.model.Role;
+import com.curso.springsecurity.service.IPermissionService;
+import com.curso.springsecurity.service.IRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @RestController
+@RequestMapping("/api/roles")
 public class RoleController {
+
+    @Autowired
+    private IRoleService roleService;
+
+    @Autowired
+    private IPermissionService permissionService;
+
+    @GetMapping
+    public ResponseEntity<List<Role>> getAllRoles() {
+        return ResponseEntity.ok(roleService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Role> getRoleById(@PathVariable Long id) {
+        Optional<Role> role = roleService.findById(id);
+        return role.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Role> createRole(@RequestBody Role role) {
+        Set<Permission> permissions = new HashSet<>();
+        Permission readPermission;
+
+        for (Permission per : role.getPermissionList()) {
+            readPermission = permissionService.findById(per.getId()).orElse(null);
+
+            if (readPermission != null) {
+                permissions.add(readPermission);
+            }
+        }
+
+        role.setPermissionList(permissions);
+        Role newRole = roleService.save(role);
+        return ResponseEntity.ok(newRole);
+    }
 }
