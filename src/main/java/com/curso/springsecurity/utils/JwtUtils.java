62dd1eac2d0c9d19cd1af969d80c6dce10145abf
VieraJulian <julianviera2019@hotmail.com>
2024-06-11 19:49:19 -0300

    Método para crear Tokens, terminado

---
+++ src/main/java/com/curso/springsecurity/utils/JwtUtils.java
@@ -0,0 +1,45 @@
+package com.curso.springsecurity.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtils {
+
+    @Value("${security.jwt.private.key}")
+    private String privateKey;
+
+    @Value("${security.jwt.user.generator}")
+    private String userGenerator;
+
+    // Creación de tokens
+    public String createToken(Authentication authentication){
+        Algorithm algorithm = Algorithm.HMAC256(privateKey);
+
+        // Queda en el context holder
+        String username = authentication.getPrincipal().toString();
+
+        String authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        return JWT.create()
+                .withIssuer(this.userGenerator)
+                .withSubject(username)
+                .withClaim("authorities", authorities)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1800000))
+                .withJWTId(UUID.randomUUID().toString())
+                .withNotBefore(new Date(System.currentTimeMillis()))
+                .sign(algorithm);
+    }
+}
