3898dccbcd35f8b583d97c454e73cddad2c486c6
VieraJulian <julianviera2019@hotmail.com>
2024-06-12 18:01:00 -0300

    Método para extraer un claim de un Token, terminado

--- src/main/java/com/curso/springsecurity/utils/JwtUtils.java
+++ src/main/java/com/curso/springsecurity/utils/JwtUtils.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -67,6 +68,9 @@ public class JwtUtils {
     }
 
     // Obtener un claim
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName) {
+        return decodedJWT.getClaim(claimName);
+    }
 
     // Obtener todos los claims
 }
